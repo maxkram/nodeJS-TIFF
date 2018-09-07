c5999dc6f51e3aa7610e71af34e92888055041dc
maxkram <max.kramer@live.com>
2018-09-07 11:14:48 -0400

    working mode

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const http = require('http');
 
-const public = require('./routes/public');
+const render = require('./lib/render');
+const { public, home, search, notFound } = require('./routes');
 
-http.createServer((req,res)=>{
-    if(req.url.match(/\.(html|css|js|png)$/)){
-        public(req,res);
-    }else if(req.url === '/'){
+http.ServerResponse.prototype.render = render;
 
-    }else if(req.url.startsWith('/search')){
-
-    }else{
-
+http.createServer((req, res) => {
+    if (req.url.match(/.(html|css|js|png)$/)) {
+        public(req, res);
+    } else if (req.url === '/') {
+        home(req, res);
+    } else if (req.url.startsWith('/search')) {
+        search(req, res);
+    } else {
+        notFound(req, res);
     }
-}).listen(3000,()=>console.log('Server is working...'));
+}).listen(3000, () => console.log('Сервер работает'));
